b11a97313e2f779e57ebaa9b6f6b966632b8de66
Rostyslav Begey <rostyk.begey@gmail.com>
2020-09-08 01:22:16 +0300

    Update user page

--- client/src/pages/User/index.jsx
+++ client/src/pages/User/index.jsx
@@ -4,16 +4,11 @@ import { useParams } from 'react-router-dom';
 
 import Page from 'app/components/Page';
 import ContestCard from 'app/components/ContestCard';
-
-import cardImage from 'assets/images/card-1.jpg';
-import useHttp from 'app/hooks/http';
-import ROUTES from 'app/utils/routes';
 import AuthContext from 'app/context/AuthContext';
 import { useContestAll } from 'app/hooks/api/contest';
 
 const UserPage = () => {
   const { id } = useParams();
-  console.log(id);
   const auth = useContext(AuthContext);
   const {
     data: { data: contests = [] } = {},
@@ -28,11 +23,12 @@ const UserPage = () => {
     <Page>
       <TablerPage.Content>
         <Grid.Row>
-          <Grid.Col lg={4} sm={12}>
+          <Grid.Col lg={4} width={12}>
             <Card className="card-profile">
               <Card.Header backgroundURL="https://preview.tabler.io/demo/photos/eberhard-grossgasteiger-311213-500.jpg" />
               <Card.Body className="text-center">
                 <img
+                  alt=""
                   className="card-profile-img"
                   src="https://preview.tabler.io/demo/faces/male/16.jpg"
                 />
@@ -40,17 +36,20 @@ const UserPage = () => {
                 <p className="mb-4">
                   Big belly rude boy, million dollar hustler. Unemployed.
                 </p>
-                <button className="btn btn-outline-primary btn-sm">
+                <button
+                  type="button"
+                  className="btn btn-outline-primary btn-sm"
+                >
                   <span className="fa fa-twitter" /> Follow
                 </button>
               </Card.Body>
             </Card>
           </Grid.Col>
-          <Grid.Col lg={8} sm={12}>
+          <Grid.Col lg={8} width={12}>
             <Grid.Row>
               {contestsQuery.isSuccess &&
                 contests.map((contest) => (
-                  <Grid.Col sm={12} md={6} lg={6} key={contest._id}>
+                  <Grid.Col width={12} md={6} lg={6} key={contest._id}>
                     <ContestCard data={contest} />
                   </Grid.Col>
                 ))}
