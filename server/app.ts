4b2da1c9c15d4a97582b511ba1e7b3069588ed03
Rostyslav Begey <rostyk.begey@gmail.com>
2020-10-24 14:14:53 +0300

    Add next server

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cloudinary from 'cloudinary';
 import cookieParser from 'cookie-parser';
 import 'reflect-metadata';
 import { getRouteInfo, InversifyExpressServer } from 'inversify-express-utils';
+import next from '../client-next/node_modules/next';
 import path from 'path';
 
 import { handleError } from './usecases/error';
@@ -14,6 +15,10 @@ mongoose.set('debug', config.mongooseDebug);
 
 const PORT = config.port;
 
+const dev = process.env.NODE_ENV === 'development';
+
+const nextApp = next({ dev, dir: path.join(__dirname, '../', 'client-next') });
+const nextAppHandle = nextApp.getRequestHandler();
 const server = new InversifyExpressServer(container, null, null);
 
 server.setConfig((app) => {
@@ -24,22 +29,6 @@ server.setConfig((app) => {
   app.disable('etag');
 });
 
-server.setErrorConfig((app) => {
-  app.use(((err: any, req: Request, res: Response, _) => {
-    return handleError(err, res);
-  }) as ErrorRequestHandler);
-});
-
-const app = server.build();
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../', 'client', 'dist')));
-
-  app.get('*', (req: Request, res: Response): void => {
-    res.sendFile(path.join(__dirname, '../', 'client', 'dist', 'index.html'));
-  });
-}
-
 (async (): Promise<void> => {
   try {
     await mongoose.connect(config.mongoUri, {
@@ -47,11 +36,37 @@ if (process.env.NODE_ENV === 'production') {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
+
     cloudinary.v2.config({
       cloud_name: config.cloudinary.cloudName,
       api_key: config.cloudinary.apiKey,
       api_secret: config.cloudinary.apiSecret,
     });
+
+    await nextApp.prepare();
+
+    server.setErrorConfig((app) => {
+      app.use(((err: any, req: Request, res: Response, _) => {
+        return handleError(err, res);
+      }) as ErrorRequestHandler);
+    });
+
+    const app = server.build();
+
+    // if (process.env.NODE_ENV === 'production') {
+    //   app.use(
+    //     '/',
+    //     express.static(path.join(__dirname, '../', 'client', 'dist')),
+    //   );
+    //
+    //   app.get('*', (req: Request, res: Response): void => {
+    //     res.sendFile(
+    //       path.join(__dirname, '../', 'client', 'dist', 'index.html'),
+    //     );
+    //   });
+    // }
+    app.get('*', (req: Request, res: Response) => nextAppHandle(req, res));
+
     app.listen(PORT, () => console.log(`Listening localhost:${PORT}`));
     const routeInfo = getRouteInfo(container);
     console.log(JSON.stringify(routeInfo, null, 2));
