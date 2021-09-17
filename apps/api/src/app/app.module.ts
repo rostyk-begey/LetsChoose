4e93d9bbfaaf9ecd80cb81b554b7aeb501e1d6bf
Rostyslav Begey <rostyk.begey@gmail.com>
2021-09-17 18:09:14 +0300

    Extract api-mongoose, api-testing-utils modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,31 +1,17 @@
 import { ApiHealthFeatureModule } from '@lets-choose/api-health-feature';
 import { ApiAuthFeatureModule } from '@lets-choose/api/auth/feature';
 import { ApiCommonServicesModule } from '@lets-choose/api/common/services';
-import { ApiConfigModule, Config } from '@lets-choose/api/config';
+import { ApiConfigModule } from '@lets-choose/api/config';
 import { ApiContestFeatureModule } from '@lets-choose/api/contest/feature';
 import { ApiGameFeatureModule } from '@lets-choose/api/game/feature';
+import { ApiMongooseModule } from '@lets-choose/api/mongoose';
 import { ApiUserFeatureModule } from '@lets-choose/api/user/feature';
 import { Module } from '@nestjs/common';
-import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ApiConfigModule,
-    MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService<Config>) => {
-        const options: MongooseModuleOptions = {
-          uri: configService.get('mongoUri', { infer: true }),
-        };
-
-        if (configService.get('environment', { infer: true }) === 'test') {
-          options.retryAttempts = 0;
-        }
-
-        return options;
-      },
-      inject: [ConfigService],
-    }),
+    ApiMongooseModule,
     ApiCommonServicesModule,
     ApiAuthFeatureModule,
     ApiUserFeatureModule,
