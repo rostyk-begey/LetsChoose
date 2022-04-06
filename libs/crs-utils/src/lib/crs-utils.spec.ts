9bfba1dd6a3a884a7c6f74b899eae5f71121a2bd
Rostyslav Begey <rostyk.begey@gmail.com>
2022-04-06 22:34:04 +0300

    chore: update workspace config

---
+++ libs/crs-utils/src/lib/crs-utils.spec.ts
@@ -0,0 +1,7 @@
+import { crsUtils } from './crs-utils';
+
+describe('crsUtils', () => {
+  it('should work', () => {
+    expect(crsUtils()).toEqual('crs-utils');
+  });
+});
---
+++ libs/crs-utils/src/lib/crs-utils.ts
@@ -0,0 +1,5 @@
+import md5 from 'md5';
+
+export function crsUtils(): string {
+  return md5('crs-utils');
+}
